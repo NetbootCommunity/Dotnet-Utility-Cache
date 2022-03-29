938b29abfaaf2bb04be9ba1029ebc18c9a47a0f5
Thomas ILLIET <contact@thomas-illiet.fr>
2022-03-29 22:49:30 +0200

    Add redis and memory implementation

---
+++ Src/Netboot.Cache.Memory/DistributedCacheExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Netboot.Cache.Domains;
+using Netboot.Cache.Extensions;
+using System;
+
+namespace Netboot.Cache.Memory
+{
+    public static class DistributedCacheExtensions
+    {
+        /// <summary>
+        /// Adds the memory cache with strongly typed read-through caching extensions.
+        /// </summary>
+        /// <param name="services">The services collection.</param>
+        /// <param name="options">The distributed cache options.</param>
+        /// <returns></returns>
+        public static IServiceCollection AddMemoryCache(this IServiceCollection services, Action<DistributedCacheOptions> options = null)
+        {
+            services.AddDistributedCache(options)
+                .AddDistributedMemoryCache();
+            return services;
+        }
+    }
+}
---
+++ Src/Netboot.Cache.Redis/DistributedCacheExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.DependencyInjection;
+using Netboot.Cache.Domains;
+using Netboot.Cache.Extensions;
+using System;
+
+namespace Netboot.Cache.Redis
+{
+    public static class DistributedCacheExtensions
+    {
+        /// <summary>
+        /// Adds the redis cache with strongly typed read-through caching extensions.
+        /// </summary>
+        /// <param name="services">The services collection.</param>
+        /// <param name="options">The distributed cache options.</param>
+        /// <returns></returns>
+        public static IServiceCollection AddMemoryCache(this IServiceCollection services, Action<RedisCacheOptions> redisOptions, Action<DistributedCacheOptions> options = null)
+        {
+            services.AddDistributedCache(options)
+                .AddStackExchangeRedisCache(redisOptions);
+            return services;
+        }
+    }
+}
